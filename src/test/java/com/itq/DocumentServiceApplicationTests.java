d852227fb9a28cedd152d9a7ad084363aadb9207
scolerad134 <gugac134@gmail.com>
2026-02-21 17:12:40 +0300

    Initial commit

---
+++ src/test/java/com/itq/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
