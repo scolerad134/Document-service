e687ad3ef54de5cdfc34d6365a619a5527421e38
scolerad134 <gugac134@gmail.com>
2026-02-22 12:47:26 +0300

    added repositories

---
+++ src/main/java/com/itq/repository/ApprovalRegistryRepository.java
@@ -0,0 +1,8 @@
+package com.itq.repository;
+
+import com.itq.entity.ApprovalRegistry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApprovalRegistryRepository extends JpaRepository<ApprovalRegistry, Long> {
+
+}
---
+++ src/main/java/com/itq/repository/HistoryRepository.java
@@ -0,0 +1,8 @@
+package com.itq.repository;
+
+import com.itq.entity.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoryRepository extends JpaRepository<History, Long> {
+
+}
